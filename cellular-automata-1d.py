e91e337d125bc2b94f3d03f0e908088b3f2d8fdb
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2017-02-24 19:59:33 +0100

    Added some comment

--- cellular-automata-1d.py
+++ cellular-automata-1d.py
@@ -1,11 +1,15 @@
+# Compact version
+# ---------------
 rule = (('0'*8 + bin(30)[2:])[-8:])[::-1]
 cells = list('0'*40 + '1' + '0'*40)
 for epoch in range(40):
     print(''.join(cells).replace("0"," ").replace("1","█"))
     cells = [cells[0]] + [rule[eval('0b' + cells[i-1]+cells[i]+cells[i+1])]
                           for i in range(1,len(cells)-1)] + [cells[-1]]
-    
-'''
+
+"""
+# Readable version
+# ----------------
 # Rule transformation into ascii/binary representation
 rule = 30
 rule = ('0'*8 + bin(rule)[2:])[-8:]
@@ -31,4 +35,4 @@ for epoch in range(n):
         code = eval('0b' + code)
         _cells.append(rule[code])
     cells = [cells[0]] + _cells + [cells[-1]]
-'''
+"""
